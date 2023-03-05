132f7a2e0e1ceed870bdcf1458c966528b89a1ee
John Doe <maaganmhub@gmail.com>
2023-03-05 06:08:34 +0200

    again

--- mqtt_emqx_cloud/pub.py
+++ mqtt_emqx_cloud/pub.py
@@ -2,19 +2,19 @@
 import time
 from umqtt.simple import MQTTClient
 
-server="z6b1d2f6.ala.us-east-1.emqxsl.com"
+server_address="z6b1d2f6.ala.us-east-1.emqxsl.com"
 ClientID = f'esp32-pub{time.time_ns()}'
 user = "MaaganmHub"
 password = "Mm12345!"
 topic = "esp32/mqtt"
 msg = b'{"msg":"hello"}'
-ssl_params = {"server_hostname": server}
+ssl_params = {"server_hostname": server_address}
 
 
 def connect():
-    client = MQTTClient(ClientID, server, 8883, user, password,0,True,ssl_params=ssl_params)
+    client = MQTTClient(ClientID, server_address, 8883, user, password,0,True,ssl_params=ssl_params)
     client.connect()
-    print('Connected to MQTT Broker "%s"' % (server))
+    print('Connected to MQTT Broker "%s"' % (server_address))
     return client
 
 def reconnect():
