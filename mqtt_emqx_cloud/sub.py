3804ea091503014d33010d2bbc7bb316cc2d8b76
John Doe <maaganmhub@gmail.com>
2023-03-05 06:08:00 +0200

    change name

--- mqtt_emqx_cloud/sub.py
+++ mqtt_emqx_cloud/sub.py
@@ -2,22 +2,22 @@
 import time
 from umqtt.simple import MQTTClient
 
-SERVER="z6b1d2f6.ala.us-east-1.emqxsl.com"
+server_address="z6b1d2f6.ala.us-east-1.emqxsl.com"
 ClientID = f'esp32-sub-{time.time_ns()}'
 user = "aa"
 password = "aa"
 topic = "esp32/mqtt"
 msg = b'{"msg":"hello"}'
-ssl_params = {"server_hostname": SERVER}
+ssl_params = {"server_hostname": server_address}
 
 def sub(topic, msg):
     print('received message %s on topic %s' % (msg, topic))
 
-def main(server=SERVER):
-    client = MQTTClient(ClientID, server, 8883, user, password,0,True,ssl_params=ssl_params)
+def main():
+    client = MQTTClient(ClientID, server_address, 8883, user, password,0,True,ssl_params=ssl_params)
     client.set_callback(sub)
     client.connect()
-    print('Connected to MQTT Broker "%s"' % (server))
+    print('Connected to MQTT Broker "%s"' % (server_address))
     client.subscribe(topic)
     while True:
         if True:
