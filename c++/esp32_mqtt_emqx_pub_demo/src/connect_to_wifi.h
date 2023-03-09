125bb783130e817d951a28fac6e1f147aed57917
John Doe <maaganmhub@gmail.com>
2023-03-09 19:05:29 +0200

    update connect to wifi methods

--- c++/esp32_mqtt_emqx_pub_demo/src/connect_to_wifi.h
+++ c++/esp32_mqtt_emqx_pub_demo/src/connect_to_wifi.h
@@ -1,8 +1,32 @@
 #include <WiFi.h>
 
 
+void Wifi_connected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("ESP32 WIFI Connected to Access Point");
+}
 
+void Get_IPAddress(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("WIFI Connected!");
+  Serial.println("IP address of Connected WIFI: ");
+  Serial.println(WiFi.localIP());
+}
+
+void Wifi_disconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Disconnected from WIFI");
+  Serial.print("Connection Lost Reason: ");
+  Serial.println(info.wifi_sta_disconnected.reason);
+  Serial.println("Reconnecting...");
+  ESP.restart();
+  // WiFi.begin(ssid, password);
+}
  void initWiFi(const char* ssid, const char* password) {
+
+
+  WiFi.onEvent(Wifi_connected,ARDUINO_EVENT_WIFI_STA_CONNECTED);
+  WiFi.onEvent(Get_IPAddress, ARDUINO_EVENT_WIFI_STA_GOT_IP);
+  WiFi.onEvent(Wifi_disconnected, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+
+
   Serial.print("Connecting to WiFi ...");
 
   WiFi.mode(WIFI_STA);
@@ -15,4 +39,3 @@
   }
   Serial.println(WiFi.localIP());
 }
-
